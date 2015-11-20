5e6d82870826074b2cda41d0e15a65d46989f1cb
Daniel Passos <daniel.hpassos@hotmail.com>
2015-11-20 12:39:27 -0200

    Adição do Script

---
+++ TodosOsInserts.sql
@@ -0,0 +1,14 @@
+﻿--insert into Clientes (nome, razao, titular, login, senha, email, telefone, celular, cep, rua, bairro, cidade, estado, pais, numero, grupo)
+ --values ( 'Daniel','Razão Teste1','Titular Teste1','daniel123','daniel123','daniel@daniel.com',
+--'1333414468','13981158715','11463240','Rua Teste1','Bairro Teste1','Cidade Teste1','Estado Teste1','Pais Teste1','123123','member');
+--values ( 'Carlos','Razão Teste2','Titular Teste2','daniel123','daniel123','carlos@carlos.com',
+--'1333414468','13981158715','11463240','Rua Teste2','Bairro Teste2','Cidade Teste2','Estado Teste2','Pais Teste2','123123','admin');
+--insert into Classes (nmClasse) values ('Liquidos'),('Solidos'),('Gases');
+
+--insert into SubClasses (nmSubClasse, classeId) values ('Gasolina',1),('Diesel',1),('Celuloide',2),('Borneol',2),('Metano',3),('Butano',3);
+
+--insert into Armazenagems (tpArmazenagem) values ('Tonel'),('Palete');
+
+--insert into TamanhoArmazems (tamanhoArmazem) values (500),(400),(600);
+
+--insert into Armazems (clienteId,usadoArmazem,tamanhoArmazemId) values (1,50,1),(1,100,2);
