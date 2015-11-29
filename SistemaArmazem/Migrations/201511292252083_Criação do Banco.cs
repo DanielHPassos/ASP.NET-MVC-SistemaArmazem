c09ab152e84ce9611fe330fb4570a47f931c938a
Daniel Passos <daniel.hpassos@hotmail.com>
2015-11-29 20:56:35 -0200

    Atualização de correção

---
+++ SistemaArmazem/Migrations/201511292252083_Criação do Banco.cs
@@ -0,0 +1,16 @@
+namespace SistemaArmazem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CriaçãodoBanco : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
